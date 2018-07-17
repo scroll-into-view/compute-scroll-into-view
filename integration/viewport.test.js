95a3776f0b488be50261c8dff16c86877da7444c
Stian Didriksen <stipsan@gmail.com>
2018-07-17 23:31:19 +0200

    add nearest test cases

--- integration/viewport.test.js
+++ integration/viewport.test.js
@@ -2,7 +2,7 @@ beforeAll(async () => {
   await page.goto('http://localhost:3000/integration/viewport')
 })
 
-const ScrollLogicalPosition = ['start', 'center', 'end']
+const ScrollLogicalPosition = ['start', 'center', 'end', 'nearest']
 
 describe('scrollMode: always', () => {
   ScrollLogicalPosition.forEach(block => {
@@ -10,6 +10,7 @@ describe('scrollMode: always', () => {
       test(`block: ${block}, inline: ${inline}`, async () => {
         const expected = await page.evaluate(
           options => {
+            window.scrollTo(0, 0)
             document.querySelector('.target').scrollIntoView(options)
             const { scrollLeft, scrollTop } = document.scrollingElement
             return { left: scrollLeft, top: scrollTop }
@@ -28,6 +29,30 @@ describe('scrollMode: always', () => {
           { block, inline }
         )
         expect(expected).toEqual(actual)
+
+        if (block === 'nearest' || inline === 'nearest') {
+          const expected = await page.evaluate(
+            options => {
+              window.scrollTo(window.innerWidth * 3, window.innerHeight * 3)
+              document.querySelector('.target').scrollIntoView(options)
+              const { scrollLeft, scrollTop } = document.scrollingElement
+              return { left: scrollLeft, top: scrollTop }
+            },
+            { block, inline }
+          )
+          const actual = await page.evaluate(
+            options => {
+              window.scrollTo(window.innerWidth * 3, window.innerHeight * 3)
+              const [{ left, top }] = window.computeScrollIntoView(
+                document.querySelector('.target'),
+                options
+              )
+              return { left, top }
+            },
+            { block, inline }
+          )
+          expect(expected).toEqual(actual)
+        }
       })
     })
   })
