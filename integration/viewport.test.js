e88c72ec2083ab98cf6508ba6e2fd85950814380
Stian Didriksen <stipsan@gmail.com>
2018-07-17 23:27:08 +0200

    start, center and end test cases

--- integration/viewport.test.js
+++ integration/viewport.test.js
@@ -1,13 +1,34 @@
-describe('app', () => {
-  beforeEach(async () => {
-    await page.goto('http://localhost:3000/integration/viewport')
-  })
+beforeAll(async () => {
+  await page.goto('http://localhost:3000/integration/viewport')
+})
 
-  it('should display a react logo', async () => {
-    const result = await page.evaluate(() => {
-      return window.computeScrollIntoView()
+const ScrollLogicalPosition = ['start', 'center', 'end']
+
+describe('scrollMode: always', () => {
+  ScrollLogicalPosition.forEach(block => {
+    ScrollLogicalPosition.forEach(inline => {
+      test(`block: ${block}, inline: ${inline}`, async () => {
+        const expected = await page.evaluate(
+          options => {
+            document.querySelector('.target').scrollIntoView(options)
+            const { scrollLeft, scrollTop } = document.scrollingElement
+            return { left: scrollLeft, top: scrollTop }
+          },
+          { block, inline }
+        )
+        const actual = await page.evaluate(
+          options => {
+            window.scrollTo(0, 0)
+            const [{ left, top }] = window.computeScrollIntoView(
+              document.querySelector('.target'),
+              options
+            )
+            return { left, top }
+          },
+          { block, inline }
+        )
+        expect(expected).toEqual(actual)
+      })
     })
-    console.log(result)
-    await expect(page).toMatch('Learn more')
   })
 })
