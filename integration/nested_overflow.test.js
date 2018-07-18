9022ebca1bf6cafc8c0c94da5bb0e41486e1a95b
Stian Didriksen <stipsan@gmail.com>
2018-07-18 17:48:06 +0200

    add nested overflow test case

---
+++ integration/nested_overflow.test.js
@@ -0,0 +1,18 @@
+beforeAll(async () => {
+  await page.goto('http://localhost:3000/integration/nested_overflow')
+})
+
+describe('scrollMode: if-needed', () => {
+  test('target in view, container out of view', async () => {
+    const actual = await page.evaluate(() => {
+      const container = document.querySelector('.container')
+      window.scrollTo(0, 0)
+      container.scrollTo(250, 250)
+      return window.computeScrollIntoView(document.querySelector('.target'), {
+        scrollMode: 'if-needed',
+      })
+    })
+    expect(actual).toHaveLength(1)
+    expect(actual).toMatchSnapshot()
+  })
+})
